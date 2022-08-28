bed9a0116654fa533ce96be14e9576cf52b3b892
Eliad Karni <EliadK995@Gmail.com>
2022-08-28 10:21:34 +0300

    Following cloth generation

--- Cloths.py
+++ Cloths.py
@@ -20,7 +20,7 @@ def load_shirts():
 
 class Cloth:
     def __init__(self, path: str, dots: dict):
-        self.texture = cv2.imread(path)
+        self.texture = cv2.imread(path, -1)
         self.dots = dots
         if "top_right" in dots and "top_left" in dots:
             self.width = math.sqrt((dots["top_right"][0] - dots["top_left"][0])**2 + (dots["top_right"][1] - dots["top_left"][1])**2)
@@ -46,15 +46,18 @@ class Shirt(Cloth):
         #rotation = cv2.getRotationMatrix2D((self.dots["top_left"][0] + (self.width/2), self.dots["top_left"][1]
         #                                    + (self.height/2)), angle, 1)
         H, W = self.texture.shape[:2]
+        x_scale, y_scale = width / self.width, height / self.height
         #ans = cv2.warpAffine(self.texture.copy(), rotation, (H, W))
-        width = int(W * (width / self.width))
-        height = int(H * (height / self.height))
+        width = int(W * x_scale)
+        height = int(H * y_scale)
 
-        return cv2.resize(self.texture, (height, width))
+        return cv2.resize(self.texture, (width,height)), \
+               int(dots["top_left"][1] - (self.dots["top_left"][1] * y_scale)),\
+               int(dots["top_left"][0] - (self.dots["top_left"][0] * x_scale))
 
 
 if __name__ == "__main__":
-    shirt = Shirt("9088.png", {"top_right": (845.0, 300.0), "top_left": (375.0, 300.0), "bot_right": (845.0, 1000.0), "bot_left": (375.0, 1000.0)})
+    shirt = Shirt("IMG02.png", {"top_right": (417.0, 82.0), "top_left": (82.0, 80.0), "bot_right": (359.0, 553.0), "bot_left": (161.0, 459.0)})
     cv2.imshow("shirt", shirt.get_sprite({"top_right": (500.0, 200.0), "top_left": (100.0, 700.0), "bot_right": (500.0, 500.0), "bot_left": (100.0, 1000.0)}))
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
--- Display.py
+++ Display.py
@@ -1,6 +1,8 @@
 from cvzone.PoseModule import PoseDetector
 import cv2
 import numpy as np
+from Cloths import Shirt
+import math
 
 RADIUS = 5
 COLOR = (0, 255, 0)
@@ -14,28 +16,42 @@ class Display:
         self.detector = PoseDetector()
         self.camera = cv2.VideoCapture(0)
 
-    def display_pic(self, shirt: np.array):
+    def display_pic(self, shirt: Shirt):
         success, img = self.camera.read()
-        y_offset = x_offset = 100
-        shirt2 = cv2.resize(shirt, [200, 200])
-        y1, y2 = y_offset, y_offset + shirt2.shape[0]
-        x1, x2 = x_offset, x_offset + shirt2.shape[1]
-
-        alpha_s = shirt2[:, :, 3] / 255.0
-        alpha_l = 1.0 - alpha_s
 
         if success:
             img2 = np.copy(img)
+            h, w, c = img.shape
             img = self.detector.findPose(img)
             lmList, bboxInfo = self.detector.findPosition(img, bboxWithHands=False)
-            if len(lmList) >= 12:
-                cv2.circle(img2, (lmList[11][1], lmList[11][2]), RADIUS, COLOR, THIKNESS)
-                cv2.circle(img2, (lmList[12][1], lmList[12][2]), RADIUS, COLOR, THIKNESS)
-                cv2.circle(img2, (lmList[23][1], lmList[23][2]), RADIUS, COLOR, THIKNESS)
-                cv2.circle(img2, (lmList[24][1], lmList[24][2]), RADIUS, COLOR, THIKNESS)
 
-            for c in range(0, 3):
-                img2[y1:y2, x1:x2, c] = (alpha_s * shirt2[:, :, c] + alpha_l * img2[y1:y2, x1:x2, c])
+            if len(lmList) >= 24:
+                    sprite, index_y, index_x = shirt.get_sprite({
+                        "top_left": (lmList[12][1], lmList[12][2]),
+                        "top_right": (lmList[11][1], lmList[11][2]),
+                        "bot_left": (lmList[24][1], lmList[24][2]),
+                        "bot_right": (lmList[23][1], lmList[23][2])
+                    })
+                    cv2.imshow("shirt", sprite)
+
+                    y1, y2 = max(0, index_y), min(index_y + sprite.shape[0], h)
+                    x1, x2 = max(0, index_x), min(index_x + sprite.shape[1], w)
+                    sprite_y_index, sprite_x_index = min(0, index_y) * -1, min(0, index_x) * -1
+                    sprite = sprite[sprite_y_index: sprite_y_index + (y2 - y1),
+                              sprite_x_index : sprite_x_index + (x2 - x1), :]
+                    alpha_s = sprite[:, :, 3] / 255.0
+                    alpha_l = 1.0 - alpha_s
+                    if alpha_l.shape == (y2 - y1, x2 - x1):
+                        for c in range(0, 3):
+                            main_pic_with_deleted_shirt = alpha_l * img2[y1:y1 + (y2 - y1), x1:x1 + (x2 - x1), c]
+                            putted_shirt = alpha_s * sprite[:, :, c]
+                            img2[y1:y2, x1:x2, c] = (main_pic_with_deleted_shirt + putted_shirt)
+
+                    # cv2.circle(img2, (lmList[11][1], lmList[11][2]), RADIUS, COLOR, THIKNESS)
+                    # cv2.circle(img2, (lmList[12][1], lmList[12][2]), RADIUS, COLOR, THIKNESS)
+                    # cv2.circle(img2, (lmList[23][1], lmList[23][2]), RADIUS, COLOR, THIKNESS)
+                    # cv2.circle(img2, (lmList[24][1], lmList[24][2]), RADIUS, COLOR, THIKNESS)
+
             cv2.imshow("Image", img2)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 self.camera.release()
@@ -45,7 +61,7 @@ class Display:
 
 
 if __name__ == "__main__":
-    shirt = cv2.imread("9088.png", -1)
+    shirt = Shirt("IMG02.png", {"top_right": (417.0, 82.0), "top_left": (82.0, 80.0), "bot_right": (359.0, 553.0), "bot_left": (161.0, 459.0)})
     displayer = Display()
     while displayer.display_pic(shirt) != EXIT_SIGNAL:
-        i = 5
+        pass
